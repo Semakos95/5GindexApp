9e5bfd737cc6c488067e6dbb88f86c9e7a2999f5
Semakos95 <geo.sem@hotmail.com>
2025-02-23 14:20:09 +0200

    create header styling

---
+++ src/app/home/aggregate-index-table/aggregate-index-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aggregate-index-table',
+  imports: [],
+  templateUrl: './aggregate-index-table.component.html',
+  styleUrl: './aggregate-index-table.component.css'
+})
+export class AggregateIndexTableComponent {
+
+}
---
+++ src/app/home/factor-analysis/factor-analysis.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-factor-analysis',
+  imports: [],
+  templateUrl: './factor-analysis.component.html',
+  styleUrl: './factor-analysis.component.css'
+})
+export class FactorAnalysisComponent {
+
+}
